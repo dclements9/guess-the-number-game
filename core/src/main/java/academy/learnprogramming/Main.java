cd92f951072a0919cabe9beaad730c601b07d507
Dylan Clements <dclements@protonmail.com>
2020-08-30 11:33:07 -0400

    Initial commit - After Constructor/Setter

---
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,37 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION="beans.xml";
+
+    public static void main(String[] args) {
+        log.info("Guess the Number Game");
+
+        // Create Context (Container)
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        // Get Number Generator bean from context (container)
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        // call method next() to get a random number
+        int number = numberGenerator.next();
+
+        // log generated number
+        log.info("number = {}", number);
+
+        // Get game bean from context (container)
+        Game game = context.getBean(Game.class);
+
+        // Call Reset Method
+        game.reset();
+
+        // close context (container)
+        context.close();
+
+    }
+}
